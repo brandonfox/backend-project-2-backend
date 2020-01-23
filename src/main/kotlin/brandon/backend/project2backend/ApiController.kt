8faf13f6e7999a772894ff3b8faed542e679abcd
brandon <brandonfox@hotmail.co.uk>
2020-01-23 23:13:29 +0700

    Added cors control and returned actual data

--- src/main/kotlin/brandon/backend/project2backend/ApiController.kt
+++ src/main/kotlin/brandon/backend/project2backend/ApiController.kt
@@ -6,22 +6,31 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@CrossOrigin(origins = ["http://localhost:8080"])
 @RequestMapping("/api")
 class ApiController @Autowired constructor(val pasteRepo: PasteRepo){
 
     @PostMapping("/paste")
     fun paste(@RequestBody data: Paste){
+        data.timePosted = System.currentTimeMillis()
+        println("Title: ${data.title}, Content: ${data.content}, Time posted: ${data.timePosted}")
         pasteRepo.save(data)
     }
 
     @PostMapping("/recents")
     fun recents(): List<Paste> {
+        println("Got request to get recent pastes")
         return pasteRepo.findTop100ByOrderByTimePostedDesc()
     }
 
     @GetMapping("/{id}")
-    fun pasteId(@PathVariable id: String): String{
-        return id
+    fun pasteId(@PathVariable id: String): Paste{
+        println("Got request to view paste with id: $id")
+        val paste = pasteRepo.findById(id.toLong())
+        if(paste.isPresent)
+            return paste.get()
+        else
+            throw IllegalArgumentException()
     }
 
 }
