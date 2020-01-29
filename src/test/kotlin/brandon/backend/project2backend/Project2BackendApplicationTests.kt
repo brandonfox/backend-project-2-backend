31c14e3dac89206c00ea8c296cd7bed21442ab61
brandon <brandonfox@hotmail.co.uk>
2020-01-29 13:22:09 +0700

    Fixed docker-compose and added some tests

--- src/test/kotlin/brandon/backend/project2backend/Project2BackendApplicationTests.kt
+++ src/test/kotlin/brandon/backend/project2backend/Project2BackendApplicationTests.kt
@@ -1,13 +1,70 @@
 package brandon.backend.project2backend
 
+import org.json.JSONObject
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import java.net.HttpURLConnection
+import java.net.URL
 
 @SpringBootTest
 class Project2BackendApplicationTests {
 
+	val url = "http://localhost:8080/api"
+
 	@Test
-	fun contextLoads() {
+	fun addPasteTest(){
+		val u = URL("$url/paste")
+		with(u.openConnection() as HttpURLConnection){
+			requestMethod = "POST"
+
+			val jsonRequest = JSONObject(mapOf(Pair("title","Test title"),Pair("content","This is a test content block")))
+
+			setRequestProperty("Content-Type","application/json")
+
+			doOutput = true
+
+			this.outputStream.write(jsonRequest.toString().toByteArray())
+
+			val response = inputStream.reader().readText()
+
+			println(response)
+
+			disconnect()
+		}
+	}
+
+	@Test
+	fun retrievePasteTest(){
+		addPasteTest()
+		val u = URL("$url/recents")
+		with(u.openConnection() as HttpURLConnection){
+			requestMethod = "POST"
+
+			setRequestProperty("Content-Type","application/json")
+
+			val response = inputStream.reader().readText()
+
+			assert(response.isNotEmpty())
+
+			disconnect()
+		}
+	}
+
+	@Test
+	fun getTestId(){
+		addPasteTest()
+		val u = URL("$url/1")
+		with(u.openConnection() as HttpURLConnection){
+			requestMethod = "GET"
+
+			setRequestProperty("Content-Type","application/json")
+
+			val response = inputStream.reader().readText()
+
+			assert(response.isNotEmpty())
+
+			disconnect()
+		}
 	}
 
 }
